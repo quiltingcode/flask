3f04d43fdd408321fa4a16426bad9b527f624fc7
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-30 17:46:02 +0000

    IMport flash and complete flash message to user

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import json
 from flask import Flask, render_template, request
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,7 +36,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print("hello")
+        flash("Thanks {}, we got your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
